22c199317ed233f368b8e97aa73e77f00d5955a7
Harry_cmartin <harrysoncampos@gmail.com>
2024-12-01 21:04:39 -0300

    Add q1difcil

--- Grafos2/dificeis/Questions/questaodificil1.py
+++ Grafos2/dificeis/Questions/questaodificil1.py
@@ -1,8 +1,25 @@
+from collections import deque
+
 class Solution:
-    def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
-        graph = defaultdict(list)
-        
-        # Construindo o grafo
-        for u, v, w in edges:
-            graph[u].append((v, w + 1))
-            graph[v].append((u, w + 1))
+    def shortestPathLength(self, graph):
+        n = len(graph)
+        # Estado inicial: (nó atual, máscara de visitação, custo acumulado)
+        queue = deque((i, 1 << i, 0) for i in range(n))  # Um estado por nó inicial
+        visited = set((i, 1 << i) for i in range(n))  # (nó atual, máscara)
+
+        # BFS para explorar todos os estados
+        while queue:
+            node, mask, cost = queue.popleft()
+
+            # Se todos os nós foram visitados
+            if mask == (1 << n) - 1:
+                return cost
+
+            # Explorar os vizinhos
+            for neighbor in graph[node]:
+                next_mask = mask | (1 << neighbor)  # Atualiza a máscara
+                if (neighbor, next_mask) not in visited:
+                    visited.add((neighbor, next_mask))
+                    queue.append((neighbor, next_mask, cost + 1))
+
+        return -1  # Nunca alcançável (não esperado para um grafo conectado)
---
+++ Grafos2/dificeis/questaodificil1.md
@@ -0,0 +1,16 @@
+## 847. Shortest Path Visiting All Nodes
+
+https://leetcode.com/problems/shortest-path-visiting-all-nodes/description/
+
+# Nível da questão:  <span style="color: yellow;">Difícil</span>
+
+## Resultados do juiz eletrônico
+![](/Grafos2/Assets/Questao1dificil_result.png)
+
+
+#### Caso 1:
+![](/Grafos2/Assets/Question1dificil_case1.png)
+
+#### Caso 2:
+![](/Grafos2/Assets/Questao1dificil_case2.png)
+
